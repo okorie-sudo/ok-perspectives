86f97b387d0d40ddf95e1493034a4bd533affb57
friday <outis8945@gmail.com>
2025-09-04 18:59:01 +0100

    reverted changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
-// next.config.mjs
-const isProd = process.env.NODE_ENV === 'production'
-
-// Set this to your repo name if you're deploying to https://username.github.io/<REPO_NAME>
-// For a user/organization site (repo named username.github.io), leave as ''.
-const repo ='ok-perspectives' // <-- change this
-const isProjectSite = true // set false if deploying to username.github.io
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true }, // required for GitHub Pages
-  trailingSlash: true,           // safer routing on static hosts
-  basePath: isProd && isProjectSite ? `/${repo}` : '',
-  assetPrefix: isProd && isProjectSite ? `/${repo}/` : '',
-}
+const nextConfig = {};
 
-export default nextConfig
+export default nextConfig;
